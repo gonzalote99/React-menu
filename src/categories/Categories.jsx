034ea7a0a43ca92330ffc99fd362a3a5d47e555c
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2024-01-04 20:13:23 +0000

    Initial commit

---
+++ src/categories/Categories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Categories({filterItems}) {
+  return(
+    <>
+    <div className='btn-container'>
+    <button className='filter-btn' onClick={() => {
+      filterItems('all');
+    }}>all</button>
+      <button className='filter-btn' onClick={() => {
+        filterItems('phone');
+      }}>phone</button>
+      <button className='filter-btn' onClick={() => {
+        filterItems('laptop');
+      }}>laptop</button>
+    </div>
+    </>
+  );
+}
